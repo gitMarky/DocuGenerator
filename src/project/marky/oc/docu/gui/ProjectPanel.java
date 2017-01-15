198356204b89ad7774a426e5313094b7da1f2e74
Mark <markh4b@hotmail.com>
2017-01-15 20:22:50 +0100

    ProjectPanel: Add title

--- src/project/marky/oc/docu/gui/ProjectPanel.java
+++ src/project/marky/oc/docu/gui/ProjectPanel.java
@@ -1,24 +1,26 @@
 package project.marky.oc.docu.gui;
 
 import java.awt.Component;
-import java.awt.Dimension;
 import java.io.File;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import project.marky.oc.docu.gui.fileChoosers.FileChooserDirectory;
-
+import project.marky.oc.docu.util.StyleConstants;
 
 /**
  * Panel that holds the project settings and generate-button.
  */
-@SuppressWarnings("serial") // no serialization intended
+@SuppressWarnings("serial")
+// no serialization intended
 public class ProjectPanel extends JPanel
 {
 	private final FileChooserDirectory _dir = new FileChooserDirectory(null);
+	final JTextField _titleField = new JTextField();
 
 
 	public ProjectPanel()
@@ -34,31 +36,68 @@ public class ProjectPanel extends JPanel
 		this.setBorder(BorderFactory.createTitledBorder("Project"));
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+		this.add(assembleTitleRow());
 		this.add(assembleButtonRow("Source"));
 		this.add(assembleButtonRow("Output"));
 	}
 
+
+	private Component assembleTitleRow()
+	{
+		final JPanel panel = subPanel();
+		final JLabel label = createLabel("Title");
+		panel.add(label);
+		panel.add(_titleField);
+		return panel;
+	}
+
+
 	private Component assembleButtonRow(final String title)
 	{
-		final JLabel label = new JLabel(title);
-		label.setPreferredSize(new Dimension(50, 0));
+		final JLabel label = createLabel(title);
 
-		final JPanel panel = new JPanel();
-
-		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		final JPanel panel = subPanel();
 		panel.add(label);
 		panel.add(new BrowseFilePanel(dir()));
 		return panel;
 	}
+
+
+	private JLabel createLabel(final String title)
+	{
+		final JLabel label = new JLabel(title);
+		label.setPreferredSize(StyleConstants.DIMENSION_PROJECT_LABEL);
+		return label;
+	}
+
+
+	private JPanel subPanel()
+	{
+		final JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		return panel;
+	}
+
 
 	private FileChooserDirectory dir()
 	{
 		return _dir;
 	}
 
-	////////////////////////////////////////////////////////////////
-	//
-	// Handle actions
 
+	// //////////////////////////////////////////////////////////////
+	//
+	// Public interface
+
+	public String getTitle()
+	{
+		return _titleField.getText();
+	}
+
+
+	public void setTitle(final String title)
+	{
+		_titleField.setText(title);
+	}
 
 }
--- tests/project/marky/oc/docu/util/StyleConstants.java
+++ tests/project/marky/oc/docu/util/StyleConstants.java
@@ -1,9 +1,14 @@
 package project.marky.oc.docu.util;
 
+import java.awt.Dimension;
+
 import javax.swing.BorderFactory;
 import javax.swing.border.Border;
 
 public class StyleConstants
 {
 	public static final Border DEFAULT_EMPTY_BORDER = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+
+	public static final Dimension DIMENSION_PROJECT_LABEL = new Dimension(50, 0);
+
 }
